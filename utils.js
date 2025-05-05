40f490161b385e2937ba6b74769df3c7e8774b2c
shubhansh-simple <shubhansh7777@gmail.com>
2025-05-05 17:20:20 +0530

    Update colspan for 'No Record Found'

--- utils.js
+++ utils.js
@@ -278,7 +278,7 @@ function prepareRecordNotFoundHTML(){
 
   let record_not_found_html = `
         <tr>
-          <th colspan=8>No Record Found</th>
+          <th colspan=9>No Recordy Found</th>
         </tr>
   `
   return record_not_found_html
